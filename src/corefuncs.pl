576797bf374239b1d3d5f4f1457b50145784cbf1
Kai Hoop <unrelatedstring@gmail.com>
2019-08-06 16:01:09 -0400

    The file exists

--- src/corefuncs.pl
+++ src/corefuncs.pl
@@ -1,1 +1,3 @@
 % implement do first, but I'll need to figure out what I'm doing about lines for that to make any sense
+
+:- use_module(lumber_bins).
--- src/lumber_bins.pl
+++ src/lumber_bins.pl
@@ -1,4 +1,6 @@
 % Currently, each bin is just a list. Eventually they'll do some sort of cycling, probably.
 
 :- module(lumber_bins,
-          []).
+          [asse/0]).
+
+asse :- assertz(y),export(y/0).
