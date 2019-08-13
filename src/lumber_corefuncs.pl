ca8ee48100cab3df5ec4e85179ac0102ad5cfca0
Kai Hoop <unrelatedstring@gmail.com>
2019-08-13 16:46:49 -0400

    Cleaned up names a bit I guess

--- src/lumber_corefuncs.pl
+++ src/lumber_corefuncs.pl
@@ -1,3 +1,3 @@
 % implement do first, but I'll need to figure out what I'm doing about lines for that to make any sense
 
-:- use_module(lumber_bins).
+:- use_module(lumber_types).
--- src/lumber_types.pl
+++ src/lumber_types.pl
@@ -1,4 +1,7 @@
-% Bins are dead, long live bins
+/*
+Lumber tries to make stack management easy with a sizeable supply of
+semantically significant subtypes. That's not to say it succeeds.
+*/
 
 :- module(lumber_types,
           []).
