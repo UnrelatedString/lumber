8aef34990bb1b6abbfd611fb61b1dd15657eb155
Kai Hoop <unrelatedstring@gmail.com>
2019-09-18 23:52:32 -0400

    Cleaned up around rationals

--- src/lumber_types.pl
+++ src/lumber_types.pl
@@ -5,14 +5,16 @@ semantically significant subtypes. That's not to say it succeeds.
 
 :- module(lumber_types,
           [lt_is/3]).
+:- use_module(library(clpfd)).
 
 lt_is(T, X, V) :-
     X =.. [T, V].
 lt_is(complex, X, c(R,0)) :-
     lt_is(real, X, R).
 lt_is(real, X, V) :-
-    lt_is(int, X, V);
-    lt_is(float, X, V);
-    lt_is(rat, X, N/D), V #= N/D.
+    lt_is(rat, X, N/D), V #= N/D;
+    lt_is(float, X, V).
+lt_is(rat, X, I/1) :-
+    lt_is(int, X, I).
 lt_is(int, X, V) :-
     lt_is(nat, X, V).
