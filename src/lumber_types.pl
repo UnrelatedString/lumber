c4214d78aebe33d3002c5bc435c242bca3f190c9
Kai Hoop <unrelatedstring@gmail.com>
2019-09-18 23:36:35 -0400

    Some basic numeric type stuff implemented

--- src/lumber_types.pl
+++ src/lumber_types.pl
@@ -5,3 +5,16 @@ semantically significant subtypes. That's not to say it succeeds.
 
 :- module(lumber_types,
           []).
+
+lt_assert_types()
+
+lt_is(T, X, V) :-
+    X =.. [T, V].
+lt_is(complex, X, c(R,0)) :-
+    lt_is(real, X, R).
+lt_is(real, X, V) :-
+    lt_is(int, X, V);
+    lt_is(float, X, V);
+    lt_is(rat, X, N/D), V #= N/D.
+lt_is(int, X, V) :-
+    lt_is(nat, X, V).
